3159d20cdf515f67b89644639fc5a04ac915a459
Dmitry Duleba <dmitryduleba@gmail.com>
2018-11-13 17:08:12 +0300

    - [REFACTOR] LineView

--- Alidade/Source/UI/Views/LineView.swift
+++ Alidade/Source/UI/Views/LineView.swift
@@ -1,32 +1,16 @@
 import UIKit
 
-// MARK: - LineView.ViewModel
-
-public extension LineView {
-
-  public struct ViewModel {
-    public let color: UIColor
-    public let start: CGPoint
-    public let end: CGPoint
-    public let width: CGFloat
-    public let pattern: [CGFloat]
-
-    public init(color: UIColor? = nil, start: CGPoint? = nil, end: CGPoint? = nil,
-                width: CGFloat? = nil, pattern: [CGFloat]? = nil) {
-      self.color = color ?? .black
-      self.start = start ?? CGPoint(x: 0.0, y: 0.0)
-      self.end = end ?? CGPoint(x: 1.0, y: 0.0)
-      self.width = width ?? 1.0
-      self.pattern = pattern ?? [10.0, 10.0]
-    }
-  }
-}
-
 // MARK: - LineView
 
 public class LineView: UIView {
 
-  public var data = ViewModel() { didSet { setNeedsDisplay() } }
+  public var startPosition = CGPoint(x: 0.0, y: 0.5) { didSet { setNeedsDisplay() } }
+  public var endPosition = CGPoint(x: 1.0, y: 0.5) { didSet { setNeedsDisplay() } }
+  public var lineWidth: CGFloat = 1.0 { didSet { setNeedsDisplay() } }
+  public var lineCap: CGLineCap = .butt { didSet { setNeedsDisplay() } }
+  public var lineJoin: CGLineJoin = .miter { didSet { setNeedsDisplay() } }
+  public var pattern: [CGFloat] = [] { didSet { setNeedsDisplay() } }
+  public var color: UIColor = .black { didSet { setNeedsDisplay() } }
 
   public override init(frame: CGRect) {
     super.init(frame: frame)
@@ -39,15 +23,10 @@ public class LineView: UIView {
   }
 
   override public func draw(_ rect: CGRect) {
-    let path = UIBezierPath()
-    path.move(to: convert(relativePoint: data.start, to: rect))
-    path.addLine(to: convert(relativePoint: data.end, to: rect))
-    path.close()
-    path.lineWidth = data.width
-    path.setLineDash(data.pattern, count: data.pattern.count, phase: 0.0)
-    data.color.setStroke()
-    path.stroke()
+    color.setStroke()
+    path(in: rect).stroke()
   }
+
 }
 
 // MARK: - Private
@@ -58,10 +37,22 @@ private extension LineView {
     backgroundColor = .clear
   }
 
+  func path(in rect: CGRect) -> UIBezierPath {
+    let path = UIBezierPath()
+    path.move(to: convert(relativePoint: startPosition, to: rect))
+    path.addLine(to: convert(relativePoint: endPosition, to: rect))
+    path.lineWidth = lineWidth
+    path.lineCapStyle = lineCap
+    path.lineJoinStyle = lineJoin
+    path.setLineDash(pattern, count: pattern.count, phase: 0.0)
+    return path
+  }
+
   func convert(relativePoint: CGPoint, to bounds: CGRect) -> CGPoint {
     let x = relativePoint.x * bounds.size.width
     let y = relativePoint.y * bounds.size.height
     let result = CGPoint(x: x, y: y)
     return result
   }
+
 }
