e7a680c2258441fa48264ab996865d388d50f792
Dmitry Duleba <dmitryduleba@gmail.com>
2018-11-14 15:16:49 +0300

    - [ADD] Logging for NSObject

---
+++ Alidade/Source/Other/Logging.swift
@@ -0,0 +1,40 @@
+//
+//  NSObject+Logging.swift
+//  Alidade
+//
+//  Created by Dmitry Duleba on 11/14/18.
+//
+
+import Foundation
+
+public extension NSObject {
+
+  public func fullClassName(shoulShowModuleName: Bool = false, dso: UnsafeRawPointer = #dsohandle) -> String {
+    return type(of: self).fullClassName(shoulShowModuleName: shoulShowModuleName, dso: dso)
+  }
+
+  public class func fullClassName(shoulShowModuleName: Bool = false, dso: UnsafeRawPointer = #dsohandle) -> String {
+    let moduleName = Module(dso).name
+    var name = description()
+    if let moduleRange = name.range(of: moduleName), moduleRange.lowerBound > name.startIndex {
+      let removeRange = name.startIndex..<moduleRange.lowerBound
+      name.removeSubrange(removeRange)
+    }
+
+    if !shoulShowModuleName, let moduleRange = name.range(of: moduleName) {
+      name.removeSubrange(moduleRange)
+    }
+
+    let result = name.components(separatedBy: CharacterSet.letters.inverted)
+      .filter { $0.count > 0 }
+      .joined(separator: ".")
+    return result
+  }
+
+}
+
+public extension Error {
+
+  public func log() { print("❌ \(localizedDescription)") }
+
+}
