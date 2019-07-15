f51cd6736c2c81337ebf8c1ebf8ae5f7808805f1
Eugene Krinitsyn <eugene.krinitsyn@gmail.com>
2019-07-15 17:50:56 +0300

    refactor(UIExtension): avoid Vetors dependency

--- Alidade/Source/Core/CGPointExtension.swift
+++ Alidade/Source/Core/CGPointExtension.swift
@@ -7,12 +7,29 @@ import struct CoreGraphics.CGPoint
 public extension CGPoint {
 
   var normalized: CGPoint {
-    let length = self.length
+    let length = sqrt(x * x + y * y)
     guard length > 0.0 else { return CGPoint(x: 1.0, y: 0.0) }
 
     return CGPoint(x: x / length, y: y / length)
   }
 
   var clampNormal: CGPoint { return clamp(CGPoint.zero, CGPoint(x: 1.0, y: 1.0)) }
-
 }
+
+extension CGPoint: Comparable {
+
+  public static func < (lhs: CGPoint, rhs: CGPoint) -> Bool {
+    return lhs.x < rhs.x && lhs.y < lhs.y
+  }
+}
+
+fileprivate extension CGPoint {
+
+  func clamp(_ min: CGPoint, _ max: CGPoint) -> CGPoint {
+    let realMin = Swift.min(min, max)
+    let realMax = Swift.max(min, max)
+    return Swift.min(Swift.max(self, realMin), realMax)
+  }
+}
+
+
