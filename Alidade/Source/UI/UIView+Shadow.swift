b9513490cebf90558dbba14e6c6ce6aeb4b9da61
Sergei Mikhan <sergei@netcosports.com>
2020-04-01 00:31:01 +0300

    fix: correct shadow setup for sketch

--- Alidade/Source/UI/UIView+Shadow.swift
+++ Alidade/Source/UI/UIView+Shadow.swift
@@ -6,80 +6,88 @@ import UIKit
 
 public extension UIView {
   private enum Associated {
-    static var frameKVO = "frame_kvo"
+    static var boundsKVO = "bounds_kvo"
   }
 
-  private var frameKVO: NSKeyValueObservation? {
+  private var boundsKVO: NSKeyValueObservation? {
     set {
       objc_setAssociatedObject(self,
-                               &Associated.frameKVO,
+                               &Associated.boundsKVO,
                                newValue,
                                .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
     get {
-      return objc_getAssociatedObject(self, &Associated.frameKVO) as? NSKeyValueObservation
+      return objc_getAssociatedObject(self, &Associated.boundsKVO) as? NSKeyValueObservation
     }
   }
 
   struct Shadow {
-    public var color: UIColor?
+    public var color: UIColor
+    public var alpha: CGFloat
+    public var x: CGFloat
+    public var y: CGFloat
     public var blur: CGFloat
-    public var opacity: CGFloat
-    public var offset: CGPoint
-    public var path: CGPath?
+    public var spread: CGFloat
 
-    public static func sketch(color: UIColor? = nil,
-                              alpha: CGFloat = 0.5,
-                              bounds: CGRect,
-                              x: CGFloat = 0, y: CGFloat = 0,
-                              blur: CGFloat = 4.0,
-                              spread: CGFloat = 0) -> Shadow {
-      var path: CGPath?
-      if spread == 0 {
-        path = nil
-      } else {
-        let rect = bounds.insetBy(dx: -spread, dy: spread)
-        path = UIBezierPath(rect: rect).cgPath
-      }
-      return Shadow(color: color, blur: blur * 0.5, opacity: alpha, offset: CGPoint(x: x, y: y), path: path)
+    func path(for bounds: CGRect) -> CGPath {
+      let rect = bounds.insetBy(dx: -spread, dy: spread)
+      return UIBezierPath(rect: rect).cgPath
     }
 
-    public init(color: UIColor? = nil, blur: CGFloat = 10.0, opacity: CGFloat = 1.0,
-                offset: CGPoint = .zero, path: CGPath? = nil) {
+    var offset: CGPoint {
+      return CGPoint(x: x, y: y)
+    }
+
+    public static func sketch(color: UIColor,
+                              x: CGFloat = 0,
+                              y: CGFloat = 0,
+                              blur: CGFloat = 4.0,
+                              spread: CGFloat = 0) -> Shadow {
+      return Shadow(color: color.withAlphaComponent(1.0),
+                    alpha: color.cgColor.alpha,
+                    x: x, y: y,
+                    blur: blur * 0.5,
+                    spread: spread)
+    }
+
+    init(color: UIColor,
+         alpha: CGFloat,
+         x: CGFloat,
+         y: CGFloat,
+         blur: CGFloat,
+         spread: CGFloat) {
       self.color = color
+      self.alpha = alpha
+      self.x = x
+      self.y = y
       self.blur = blur
-      self.opacity = opacity
-      self.offset = offset
-      self.path = path
+      self.spread = spread
     }
 
   }
 
   var shadow: Shadow {
     get {
-      return Shadow(color: layer.shadowColor.map { UIColor(cgColor: $0) },
-                    blur: layer.shadowRadius,
-                    opacity: layer.shadowOpacity.cg,
-                    offset: CGPoint(x: layer.shadowOffset.width, y: layer.shadowOffset.height),
-                    path: layer.shadowPath)
+      return Shadow(color: .black, alpha: 0.0, x: 0.0, y: 0.0, blur: 0.0, spread: 0.0)
     }
     set {
-      if !frame.isEmpty { applyShadow() }
-      if frameKVO == nil {
-        frameKVO = observe(\.frame) { [weak self] view, _ in
+      if !frame.isEmpty { applyShadow(shadow: newValue) }
+      if boundsKVO == nil {
+        boundsKVO = observe(\.bounds) { [weak self] view, _ in
           if view.frame.isEmpty == false {
-            self?.applyShadow()
+            self?.applyShadow(shadow: newValue)
           }
         }
       }
     }
   }
 
-  private func applyShadow() {
-    layer.shadowColor = shadow.color?.cgColor
+  private func applyShadow(shadow: Shadow) {
+    layer.shadowColor = shadow.color.cgColor
     layer.shadowRadius = shadow.blur
-    layer.shadowPath = shadow.path
-    layer.shadowOpacity = Float(shadow.opacity)
+    layer.shadowPath = shadow.path(for: bounds)
+    layer.shadowOpacity = Float(shadow.alpha)
     layer.shadowOffset = CGSize(width: shadow.offset.x, height: shadow.offset.y)
   }
 }
+
